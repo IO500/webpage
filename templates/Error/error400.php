0b681d25156d63fd82bab05f4cd0939dcc18c016
Jean Luca Bez <jeanlucabez@gmail.com>
2020-06-15 00:03:34 -0300

    Configure 404 error page

--- templates/Error/error400.php
+++ templates/Error/error400.php
@@ -31,8 +31,7 @@ if (Configure::read('debug')) :
 $this->end();
 endif;
 ?>
-<h2><?= h($message) ?></h2>
-<p class="error">
-    <strong><?= __d('cake', 'Error') ?>: </strong>
-    <?= __d('cake', 'The requested address {0} was not found on this server.', "<strong>'{$url}'</strong>") ?>
-</p>
+
+<div id="error-404">
+    
+</div>

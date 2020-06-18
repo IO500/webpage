23d0dc76726a0336a473c387a8814529c120ec09
Jean Luca Bez <jeanlucabez@gmail.com>
2020-06-18 17:56:02 -0300

    Include error 500 page

--- templates/Error/error500.php
+++ templates/Error/error500.php
@@ -35,8 +35,7 @@ if (Configure::read('debug')) :
     $this->end();
 endif;
 ?>
-<h2><?= __d('cake', 'An Internal Error Has Occurred') ?></h2>
-<p class="error">
-    <strong><?= __d('cake', 'Error') ?>: </strong>
-    <?= h($message) ?>
-</p>
+
+<div id="error-500">
+    
+</div>

088c00a90dee3c8e80bd8ee45d73bba6b935109c
John Bent <johnbent@james-smith.dreamhost.com>
2020-07-18 17:50:32 -0700

    small edits in documentation

--- install.sh
+++ install.sh
@@ -8,3 +8,4 @@ php composer.phar install
 cp config/app_local.example.php config/app_local.php
 
 echo "NOTICE: You should now configure the database connection in the config/app_local.php file."
+echo "Specifically these 4: 'host', 'username', 'password' and 'database' in Datasources/default"

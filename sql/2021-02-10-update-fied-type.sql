baba1d9cf6107b5bd15ef1f47072317fc5eb26b3
Jean Luca Bez <jeanlucabez@gmail.com>
2021-02-10 11:32:27 -0300

    this closes #72 by fixing the field type

---
+++ sql/2021-02-10-update-fied-type.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `submissions` CHANGE `information_md_nodes` `information_md_nodes` INT NULL DEFAULT NULL;
+ALTER TABLE `submissions` CHANGE `information_md_storage_devices` `information_md_storage_devices` INT NULL DEFAULT NULL;
+ALTER TABLE `submissions` CHANGE `information_ds_nodes` `information_ds_nodes` INT NULL DEFAULT NULL;
+ALTER TABLE `submissions` CHANGE `information_ds_storage_devices` `information_ds_storage_devices` INT NULL DEFAULT NULL;

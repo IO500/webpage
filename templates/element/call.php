8d389eef5070f41625e7c37804eb533b3044128c
Jean Luca Bez <jeanlucabez@gmail.com>
2021-05-22 12:35:57 -0700

    Include missing file

---
+++ templates/element/call.php
@@ -0,0 +1,3 @@
+<div class="call">
+	Please submit your IO500 results for the upcoming <?php echo $this->Html->link("ISC'21", ['controller' => 'pages', 'action' => 'display', 'bof-isc21']); ?> list, which will be presented at our BOF. See the <?php echo $this->Html->link('Call for Submissions', ['controller' => 'pages', 'action' => 'display', 'cfs' ]); ?> for details.
+</div>

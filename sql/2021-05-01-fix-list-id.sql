a08de9c68666d284c0cdf1cf48a1cf8193b80483
Jean Bez <jeanlucabez@gmail.com>
2021-07-02 08:09:59 -0700

    Fix wrong list ID for new submissions

---
+++ sql/2021-05-01-fix-list-id.sql
@@ -0,0 +1,4 @@
+UPDATE list_isc21_io500 SET listing_id = 24;
+UPDATE list_isc21_10node SET listing_id = 25;
+UPDATE list_isc21_full SET listing_id = 27;
+UPDATE list_isc21_historical SET listing_id = 28;

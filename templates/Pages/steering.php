d37be1379388c9a8089c8ac4c822a0c022ea8e3a
Julian Kunkel <JulianKunkel@users.noreply.github.com>
2021-07-08 08:02:23 +0200

    Updated my own affil

--- templates/Pages/steering.php
+++ templates/Pages/steering.php
@@ -39,7 +39,7 @@
     <ul>
       <li>Andreas Dilger - Whamcloud/DDN</li>
       <li>Dean Hildebrand - Google</li>
-      <li><a class="link" href="https://hps.vi4io.org/about/people/julian_kunkel">Julian Kunkel - University of Reading</a></li>
+      <li><a class="link" href="https://hps.vi4io.org/about/people/julian_kunkel">Julian Kunkel - Georg-August-Universität Göttingen/GWDG</a></li>
       <li><a class="link" href="https://cfwebprod.sandia.gov/cfdocs/CompResearch/templates/insert/profile.cfm?gflofst">Jay Lofstead - Sandia National Laboratories</a></li>
       <li><a class="link" href="http://www.markomanolis.com/site/index.html">George Markomanolis - CSC - IT Center For Science Ltd</a></li>
     </ul>

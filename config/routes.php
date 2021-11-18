c1f79fa2bc5531d1e19792ebba4ee7de8a5481b5
Jean Luca Bez <jeanlucabez@gmail.com>
2021-11-18 07:57:48 -0800

    Update routes.php (#190)

--- config/routes.php
+++ config/routes.php
@@ -67,7 +67,7 @@ $routes->scope('/', function (RouteBuilder $builder) {
         [
             'controller' => 'Listings',
             'action' => 'list',
-            'isc21',
+            'sc21',
             'io500'
         ]
     );

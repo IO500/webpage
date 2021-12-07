4b0d993967a2480207a9a693f05f005ea9303401
Andreas Dilger <adilger@whamcloud.com>
2021-12-07 15:39:45 -0700

    Create 2021-12-07-update-MCK-HPC-name-again.sql
    Swap system and institution name for MCK-HPC, per Zeeshan Qureshi.

---
+++ sql/2021-12-07-update-MCK-HPC-name-again.sql
@@ -0,0 +1,2 @@
+UPDATE submissions SET information_institution = 'Raytheon' WHERE information_system = 'Ratheon';
+UPDATE submissions SET information_system = 'MCK-HPC' WHERE information_system = 'Ratheon';

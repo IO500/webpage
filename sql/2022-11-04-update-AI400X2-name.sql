d9154ff173a56984f8345127550efb131d9937f0
Andreas Dilger <adilger@whamcloud.com>
2022-11-04 18:32:45 -0600

    sql: fix AI400X2 system names
    Per Shuichi Ihara, split the two AI400X2 results into systems named
    AI400X2-I and AI400X2-II since they are different systems.

---
+++ sql/2022-11-04-update-AI400X2-name.sql
@@ -0,0 +1,2 @@
+UPDATE submissions SET information_system = 'AI400X2-I' WHERE information_storage_vendor = 'DDN' AND information_system = 'AI400X2' AND id = 615;
+UPDATE submissions SET information_system = 'AI400X2-II' WHERE information_storage_vendor = 'DDN' AND information_system = 'AI400X2' AND id = 595;

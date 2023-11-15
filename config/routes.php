08a04fbb6689e86678871e2aea56da56d3541ca3
Jean Luca Bez <jeanlucabez@gmail.com>
2023-11-15 11:29:08 -0700

    Update routes.php for SC23 release

--- config/routes.php
+++ config/routes.php
@@ -67,7 +67,7 @@ $routes->scope('/', function (RouteBuilder $builder) {
         [
             'controller' => 'Listings',
             'action' => 'list',
-            'isc23',
+            'sc23',
             'io500',
         ]
     );

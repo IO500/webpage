d0af286787c42676ad2d6bdd3f5594f0cf994ef0
Andreas Dilger <adilger@whamcloud.com>
2025-07-22 09:22:16 -0600

    about: add brief description of phases
    Add a brief description of the phases of the IO500 benchmark
    to the about page based on the IO500 Random Read Phase Proposal.

--- templates/Pages/about.php
+++ templates/Pages/about.php
@@ -29,10 +29,97 @@
 
     <ul>
         <li><strong>IOEasy</strong>: Applications with well optimized I/O patterns</li>
+            <ul>
+            <li><strong>ior-easy-write</strong>:
+                Each process creates and writes to one file at the maximum write
+                throughput of the storage system. Each process writes the same
+                size of file as that of the rank which was able to write the
+                most data when the 300s stonewall timer is hit, so that every
+                process writes the same amount of data.
+            </li>
+            <li><strong>ior-easy-read</strong>:
+                Each process reads the entirety of one file created by a
+                process on a different node during the
+                <strong>ior-easy-write</strong> phase.
+            </li>
+            <li><strong>ior-rnd4k-easy-read</strong>:
+                Each process reads random 4k blocks from one file created by
+                another process on a different node in the
+                <strong>ior-easy-write</strong> phase until the 300s stonewall
+                timer is hit.
+            </li>
+            </ul>
         <li><strong>IOHard</strong>: Applications that require a random workload</li>
+            <ul>
+            <li><strong>ior-hard-write</strong>:
+                Create one shared file and write interleaved 47008-byte records
+                to the file at the maximum write throughput of the storage
+                system. The number of records written by each process is
+                determined by the rank which was able to write the most records
+                when the 300s stonewall timer is hit, so that every process
+                writes the same amount of data.
+            </li>
+            <li><strong>ior-hard-read</strong>:
+                Each process reads interleaved 47008-byte records from the file
+                created by the <strong>ior-hard-write</strong> phase with an
+                offset that ensures each node is reading records written by a
+                process on a different node.
+            </li>
+            </ul>
         <li><strong>MDEasy</strong>: Metadata/small objects</li>
+            <ul>
+            <li><strong>mdtest-easy-write</strong>:
+                Each process creates a unique directory and creates zero-byte
+                files within that directory at the maximum rate possible.
+                Each process creates the same number of files as the rank that
+                created the most files when the 300s stonewall timer is hit.
+            </li>
+            <li><strong>mdtest-easy-stat</strong>:
+                Each process fetches the file attributes of the files created
+                by a process on a different node during the
+                <strong>mdtest-easy-write</strong> phase.
+            </li>
+            <li><strong>mdtest-easy-delete</strong>:
+                Each process deletes all of the files and directory created by
+                a process on a different node during the
+                <strong>mdtest-easy-write</strong> phase.
+            </li>
+            </ul>
         <li><strong>MDHard</strong>: Small files (3901 bytes) in a shared directory</li>
+            <ul>
+            <li><strong>mdtest-hard-write</strong>:
+                A single directory is created and each process creates
+                3901-byte files with a unique name within that directory at
+                the maximum rate possible. Each process creates the same
+                number of files as of the rank which was able to write the
+                most data within the 300s stonewall time period.
+            </li>
+            <li><strong>mdtest-hard-stat</strong>:
+                Each process fetches the file attributes of the files created
+                by a process on a different node during the
+                <strong>mdtest-hard-write</strong> phase.
+            </li>
+            <li><strong>mdtest-hard-read</strong>:
+                Each process reads the 3901-byte data of the files created
+                by a process on a different node during the previous
+                <strong>mdtest-hard-write/-stat</strong> phases.
+            </li>
+            <li><strong>mdtest-hard-delete</strong>:
+                Each process deletes all of the files created by a process on
+                a different node during the previous
+                <strong>mdtest-hard-write/-stat/-read</strong> phases.
+            </li>
+            </ul>
         <li><strong>Find</strong>: Finding relevant objects based on patterns</li>
+            <ul>
+            <li><strong>find-easy</strong>:
+                Scans the full set of files created by all of the
+                <strong>*-write</strong> phases, and look for files created
+                after the <strong>mdtest-easy-write</strong> phase with a
+                filename containing the string '01' somewhere within it.
+                Reports the total number of such files found.
+            </li>
+            </ul>
     </ul>
 
     <p>

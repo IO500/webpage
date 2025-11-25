3e9750744f9b57af4f7520f020e95fe092d415fe
Andreas Dilger <adilger@dilger.ca>
2025-11-25 16:33:50 -0700

    bof: add SC25 slide deck

--- templates/Pages/bof_sc25.php
+++ templates/Pages/bof_sc25.php
@@ -106,5 +106,5 @@
         The outcome of this BoF will steer the direction of community efforts.
     </p>
 
-    <!-- insert presentation link <a class="link" href="/files/SC25_IO500_Presentation.pdf" target="_blank">BOF Slides</a> -->
+    <a class="link" href="/files/SC25_IO500_Presentation.pdf" target="_blank">BOF Slides</a>
 </div>
